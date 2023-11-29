38c0f5da8280561372147d2c29bdaa881fb324ac
markoton <markotoncanovic@gmail.com>
2023-11-29 14:55:38 +0100

    set first rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent, pathMatch: 'full'},
+  // {path:'home', component: HomeComponent},
+  // {path:'about/:id', component: AboutComponent},
+  // {path:'edit/:id', component: EditComponent},
+  // {path:'add-user', component: AddUserComponent},
+  // {path:'**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
