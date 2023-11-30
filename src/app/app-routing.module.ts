0bc575dc9a8a1892bc00935e0d7e8bc8b69e7888
markoton <markotoncanovic@gmail.com>
2023-11-30 15:10:47 +0100

    add two router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductsListPageComponent } from './products/pages/products-list-page/products-list-page.component';
+import { ProductsPageComponent } from './products/pages/products-page/products-page.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent, pathMatch: 'full'},
-  // {path:'home', component: HomeComponent},
-  // {path:'about/:id', component: AboutComponent},
+  {path:'products', component: ProductsListPageComponent},
+  {path:'products/:id', component: ProductsPageComponent},
   // {path:'edit/:id', component: EditComponent},
   // {path:'add-user', component: AddUserComponent},
   // {path:'**', component: HomeComponent},
