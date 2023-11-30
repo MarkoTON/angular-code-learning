20a4fe72e8da9dff4cbf430859fb6259a858cf5f
markoton <markotoncanovic@gmail.com>
2023-11-30 13:55:58 +0100

    add new components and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { FilterComponent } from './filter/filter.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductComponent } from './product/product.component';
+import { ProductsFilterComponent } from './products/components/products-filter/products-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FilterComponent,
-    ProductsComponent,
-    ProductComponent
+    ProductsFilterComponent
   ],
   imports: [
     BrowserModule,
