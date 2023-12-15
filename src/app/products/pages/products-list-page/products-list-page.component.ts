e0706940245c2c4ab42e4cf115a8c884f2d0a482
markoton <markotoncanovic@gmail.com>
2023-12-15 14:47:20 +0100

    get data and assigne to products

--- src/app/products/pages/products-list-page/products-list-page.component.ts
+++ src/app/products/pages/products-list-page/products-list-page.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../service/products.service';
-
 @Component({
   selector: 'app-products-list-page',
   templateUrl: './products-list-page.component.html',
   styleUrls: ['./products-list-page.component.scss']
 })
 export class ProductsListPageComponent {
+  products: Array<any> = [];
 
-  constructor(products: ProductsService){
+  constructor(private productsHttp: ProductsService){
   }
 
+  ngOnInit(): void {
+    console.log(this.productsHttp.getAll())
+    this.productsHttp.getAll().subscribe((data)=> {
+      this.products = data.products;
+      console.log(this.products);
+    })
+  }
 }
