91d5a6edf069d13752fdeeb0d0f743978aa885ff
markoton <markotoncanovic@gmail.com>
2023-12-18 13:30:28 +0100

    adding interface

--- src/app/products/pages/products-list-page/products-list-page.component.ts
+++ src/app/products/pages/products-list-page/products-list-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../service/products.service';
+import { Product } from '../../product';
 @Component({
   selector: 'app-products-list-page',
   templateUrl: './products-list-page.component.html',
   styleUrls: ['./products-list-page.component.scss']
 })
 export class ProductsListPageComponent {
-  products: Array<any> = [];
+  products: Product[] = [];
 
   constructor(private productsHttp: ProductsService){
   }
@@ -14,7 +15,9 @@ export class ProductsListPageComponent {
   ngOnInit(): void {
     console.log(this.productsHttp.getAll())
     this.productsHttp.getAll().subscribe((data)=> {
+      console.log(data);
       this.products = data.products;
+      console.log(this.products)
     })
   }
 }
--- src/app/products/service/products.service.ts
+++ src/app/products/service/products.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ProductsResponse } from '../product';
 
 @Injectable()
 export class ProductsService {
   constructor(private http: HttpClient) {}
 
-  getAll(): Observable<any> {
+  getAll(): Observable<ProductsResponse> {
     const apiUrl = `https://dummyjson.com/products`;
-    return this.http.get(apiUrl);
+    let products = this.http.get(apiUrl)
+    console.log(products);
+    return this.http.get<ProductsResponse>(apiUrl);
   }
 }
